373c947123c7fa62615db8f7417935088b287de5
Abidstic <abid71@student.sust.edu>
2025-10-16 12:23:52 +0600

    updated data base dir path allocation

--- const.py
+++ const.py
@@ -1,3 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+# This loads all variables from .env file
+load_dotenv()
 
 DATA_BASE_DIR = os.environ.get('DATA_BASE_DIR')
+
+if not DATA_BASE_DIR:
+    print("Warning: DATA_BASE_DIR not set in .env file")
+    DATA_BASE_DIR = './data'  # Fallback to relative path
